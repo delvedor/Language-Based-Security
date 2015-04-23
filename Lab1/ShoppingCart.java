50d2d1db256c21cff4a9bc8674fdaf70f2be9fa7
delvedor <tommydelved@gmail.com>
2015-04-23 12:35:25 +0200

    Lab1: ToCToU

---
+++ Lab1/ShoppingCart.java
@@ -0,0 +1,78 @@
+import java.io.*;
+ 
+class ShoppingCart{
+	public static void main (String[] args) throws Exception{
+		Wallet wallet = new Wallet();
+		Pocket pocket = new Pocket();
+		System.out.println("Your balance is: "+wallet.getBalance());
+		System.out.println(Store.asString());
+
+		System.out.print("What do you want to buy? <insert a product name, e.g. pen> :");
+		String productName = getConsInput();
+		System.out.println("product: "+productName);
+		int prodPrice=Store.products.get(productName);
+
+		if (wallet.getBalance() < prodPrice){
+			System.out.println("Sorry, you don´ t have enough money ");			
+			System.exit(1);
+		}
+
+		wallet.setBalance(wallet.getBalance() - prodPrice);
+		System.out.println("Your new balance is: "+wallet.getBalance());
+		pocket.addProduct(productName);
+
+	}
+
+	private static String getConsInput(){
+		String strout = "";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+         	strout = br.readLine();
+      	} catch (IOException ioe) {
+         	System.out.println("IO error trying to read the product name!");
+         	System.exit(1);
+      	}
+		return strout; 
+	}
+} 
+
+/**
+ * This code have the problem that the currentBalance is read at the beginning and is not check anymore.
+ * So an attacker can bay a care and after a pen if launches the program at the same time and will get the car "for free".
+ */
+/*class ShoppingCart{
+	public static void main (String[] args) throws Exception{
+		Wallet wallet = new Wallet();
+		Pocket pocket = new Pocket();
+		int currentBalance = wallet.getBalance();
+		System.out.println("Your balance is: "+currentBalance);
+		System.out.println(Store.asString());
+
+		System.out.print("What do you want to buy? <insert a product name, e.g. pen> :");
+		String productName = getConsInput();
+		System.out.println("product: "+productName);
+		int prodPrice=Store.products.get(productName);
+
+		if (currentBalance < prodPrice){
+			System.out.println("Sorry, you don´ t have enough money ");			
+			System.exit(1);
+		}
+
+		wallet.setBalance(currentBalance - prodPrice);
+		System.out.println("Your new balance is: "+wallet.getBalance());
+		pocket.addProduct(productName);
+
+	}
+
+	private static String getConsInput(){
+		String strout = "";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+         	strout = br.readLine();
+      	} catch (IOException ioe) {
+         	System.out.println("IO error trying to read the product name!");
+         	System.exit(1);
+      	}
+		return strout; 
+	}
+} */
---
+++ Lab1/Store.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Store {
+        /**
+        * Store.products is a HashMap with the products and their prices.
+        * You can access them as:
+        *      Integer price = Store.products.get("book");
+        */
+        public final static Map<String,Integer> products= new HashMap<String,Integer>();
+		
+        static {
+	       products.put("candies", 1);
+	       products.put("car", 30000);
+	       products.put("pen", 40);
+	       products.put("book", 100);
+        }
+
+        /**
+        * Returns the product list as a String. 
+        *
+        * @return    product list
+        */
+        public static String asString(){
+	       String res = "";
+	       for (String p : products.keySet()) {
+	               res += p + "\t" + products.get(p) + "\n";
+	       }
+ 	      return res;
+        }
+}
+		
